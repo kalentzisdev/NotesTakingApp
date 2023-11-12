39d7f33ddbd93948471b224be9a8c62a246799ec
Antonis Kalentzis <kalentzisdev@gmail.com>
2023-11-12 04:44:41 +0200

    Update Form1.cs

--- NotesTakingApp/Form1.cs
+++ NotesTakingApp/Form1.cs
@@ -24,7 +24,7 @@ namespace NotesTakingApp
                 Directory.CreateDirectory(notesDirectory);
             }
 
-            PopulateDataGridView();
+            //PopulateDataGridView();
         }
 
 
